f1d9c281fed66469e8edc579ce1e305a6bf1c2cd
sy0223lee <89008921+sy0223lee@users.noreply.github.com>
2024-02-09 17:53:46 +0900

    [Silver IV] Title: 스택, Time: 384 ms, Memory: 20520 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -6,57 +6,38 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
       
         int cmdCount = Integer.parseInt(br.readLine());
-        int[] stack = new int[cmdCount];  
+        int[] stack = new int[cmdCount];
+        int index = -1;  
 
         for(int i=0; i<cmdCount; i++){
             String[] cmd = br.readLine().split(" ");
 
             switch (cmd[0]) {
                 case "push":
-                    for(int j=0; j<cmdCount; j++){
-                        if(stack[j] == 0){
-                            stack[j] = Integer.parseInt(cmd[1]);
-                            break;
-                        }
-                    }
+                    stack[++index] = Integer.parseInt(cmd[1]);
                     break;
                 case "pop":
-                    if(stack[0] == 0)
+                    if(index < 0)
                         System.out.println(-1);
                     else{
-                        for(int j=0; j<cmdCount; j++){
-                            if(stack[j] == 0){
-                                System.out.println(stack[j-1]);
-                                stack[j-1] = 0;
-                                break;
-                            }
-                        }
+                        System.out.println(stack[index]);
+                        stack[index--] = 0;
                     }
                     break;
                 case "size":
-                    for(int j=0; j<cmdCount; j++){
-                        if(stack[j] == 0){
-                            System.out.println(j);
-                            break;
-                        }
-                    }
+                    System.out.println(index + 1);
                     break;
                 case "empty":
-                    if(stack[0] == 0)
+                    if(index < 0)
                         System.out.println(1);
                     else
                         System.out.println(0);
                     break;
                 case "top":
-                    if(stack[0] == 0)
+                    if(index < 0)
                         System.out.println(-1);
                     else{
-                        for(int j=1; j<cmdCount; j++){
-                            if(stack[j] == 0){
-                                System.out.println(stack[j-1]);
-                                break;
-                            }
-                        }
+                        System.out.println(stack[index]);
                     }
                     break;            
                 default:
