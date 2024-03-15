3409b5673981846bdeae532940e80b9cc93f5856
sy0223lee <89008921+sy0223lee@users.noreply.github.com>
2024-03-15 22:54:50 +0900

    [Silver II] Title: 스택 수열, Time: 844 ms, Memory: 3572 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+int main(){
+    int n;
+    vector<int> nums;    // 1부터 n까지
+    vector<int> seq; // 주어진 수열
+    vector<char> result; // +, - 결과
+    stack<int> st;  // 스택
+
+    cin >> n;
+    for(int i=1; i<=n; i++){
+        int tmp;
+        cin >> tmp;
+        seq.push_back(tmp);
+        nums.push_back(i);
+    }
+
+    st.push(0);
+    for(int i=0; i<n; i++){
+        if(st.top() == seq[i]){ // 스택.top이랑 동일하면 pop
+            st.pop();
+            result.push_back('-');
+        }else if(st.top() > seq[i]){    // 스택.top이 더 크면 불가능
+            cout << "NO" << endl;
+            return 0;
+        }else{  // 스택.top보다 크면 수열[i]까지 push
+            for(int j=nums[0]; j<=seq[i]; j++){
+                st.push(j);
+                result.push_back('+');
+                nums.erase(nums.begin());
+            }
+            st.pop();
+            result.push_back('-');
+        }
+    }
+    for(char c : result){
+        cout << c << "\n";    // 시간초과 발생으로 endl을 \n로 수정
+    }
+    return 0;
+}
