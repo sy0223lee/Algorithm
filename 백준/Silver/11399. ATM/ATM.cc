40b15993a815890d6b84ea22d52bbb85e305391a
sy0223lee <89008921+sy0223lee@users.noreply.github.com>
2024-03-16 21:35:21 +0900

    [Silver IV] Title: ATM, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int N;  // 사람의 수
+    vector<int> P;  // 각 사람이 인출하는데 걸리는 시간
+    vector<int> wait;   // 각 사람이 기다려야 하는 시간
+    int time, total = 0;
+
+    cin >> N;
+    for(int i=0; i<N; i++){
+        cin >> time;
+        P.push_back(time);
+    }
+
+    // 빨리 걸리는 사람이 먼저 수행하면 총 시간도 최소가 됨
+    sort(P.begin(), P.end());
+    time = 0;
+    for(int i=0; i<N; i++){
+        time += P[i];
+        total += time;
+    }
+    cout << total << endl;
+
+    return 0;
+}
