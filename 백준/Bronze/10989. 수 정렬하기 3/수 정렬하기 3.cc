9cc445f4486e50843ed95523fec814e01d31e681
sy0223lee <89008921+sy0223lee@users.noreply.github.com>
2024-03-22 11:25:17 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1824 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false); // cin, cout으로 인한 시간초과 해결
+    int N = 0;
+    int num = 0;
+    int count[10001] = {0,};
+
+    cin >> N;
+    for(int i=0; i<N; i++){
+        cin >> num;
+        count[num]++;
+    }
+
+    for(int i=1; i<10001; i++){
+        if(count[i] != 0){
+            for(int j=0; j<count[i]; j++){
+                cout << i << "\n";
+            }
+        }
+    }
+    return 0;
+}
