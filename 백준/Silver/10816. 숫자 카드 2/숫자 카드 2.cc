43da9c2a89e3a09d14a68e2414b325003f2fd9f9
sy0223lee <89008921+sy0223lee@users.noreply.github.com>
2024-03-28 18:08:10 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 308 ms, Memory: 5224 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -1,33 +1,30 @@
 #include <iostream>
 #include <algorithm>
-#include <map>
+#include <vector>
 using namespace std;
-
+// upper_bound와 lower_bound를 이용한 이분탐색
 int main(){
     // 시간 초과 해결
     ios::sync_with_stdio(0);
     cin.tie(nullptr);
 
-	map<int, int> m;
 	int N = 0, M = 0, cardNum = 0;
-	
-    cin >> N;
+	vector<int> v;
+
+	cin >> N;
 	while (N--){
 		cin >> cardNum;
-		m[cardNum]++;
+		v.push_back(cardNum);
 	}
+    // 이분탐색을 위해 정렬
+	sort(v.begin(), v.end());
 
 	cin >> M;
 	while (M--){
 		cin >> cardNum;
-		auto it = m.find(cardNum);
-
-		if (it != m.end()){
-			cout << it->second << " ";
-        }
-		else{
-			cout << "0 ";
-        }
+        // upper_bound() - lower_bound()를 하여 등장한 횟수를 구할 수 있음
+		cout << upper_bound(v.begin(), v.end(), cardNum)
+			- lower_bound(v.begin(), v.end(), cardNum) << " ";
 	}
     return 0;
 }
