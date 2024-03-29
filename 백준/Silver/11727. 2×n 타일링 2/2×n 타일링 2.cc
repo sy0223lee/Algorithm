e7486a23f89baf7863552e7ac7723af0e49ecf83
sy0223lee <89008921+sy0223lee@users.noreply.github.com>
2024-03-29 10:13:25 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    int n;  // 2*n 크기의 직사각형
+    vector<int> dp;
+    /* 개수가 너무 많아서 long long으로도 감당 X
+        따라서 10007로 계속 나눠주면서 더해야 함 */
+    cin >> n;
+    dp.push_back(1);
+    dp.push_back(3);    
+    for(int i=2; i<n; i++){
+        dp.push_back((dp[i-1] + 2*dp[i-2]) % 10007);
+    }    
+    cout << dp[n-1] << endl;
+    return 0;
+}
