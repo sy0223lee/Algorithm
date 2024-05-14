5e6a9598071b1f95b02b7047e7bc01260db864c2
sy0223lee <89008921+sy0223lee@users.noreply.github.com>
2024-05-14 17:04:12 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 24 ms, Memory: 3040 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.cc
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.cc
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
 
 int N, M, count;
@@ -8,21 +9,30 @@ bool visited[601][601];
 int dx[4] = {0, 1, 0, -1};
 int dy[4] = {1, 0, -1, 0};
 
-void DFS(int x, int y){
+void BFS(int x, int y){
+    queue<pair<int, int>> q;
+    q.push({x, y});
     visited[x][y] = true;
 
-    for(int i=0; i<4; i++){
-        int nx = x + dx[i];
-        int ny = y + dy[i];
+    while(!q.empty()){
+        int cx = q.front().first;
+        int cy = q.front().second;
+        q.pop();
 
-        if(nx < 0 || nx >= N || ny < 0 || ny >= M){
-            continue;
-        }
-        if(map[nx][ny] != 'X' && !visited[nx][ny]){
-            if(map[nx][ny] == 'P'){
-                count++;
+        for(int i=0; i<4; i++){
+            int nx = cx + dx[i];
+            int ny = cy + dy[i];
+
+            if(nx < 0 || nx >= N || ny < 0 || ny >= M){
+                continue;
             }
-            DFS(nx, ny);
+            if(map[nx][ny] != 'X' && !visited[nx][ny]){
+                if(map[nx][ny] == 'P'){
+                    count++;
+                }
+                visited[nx][ny] = true;
+                q.push({nx, ny});
+            }
         }
     }
 }
@@ -42,7 +52,7 @@ int main(){
         }
     }
 
-    DFS(x, y);
+    BFS(x, y);
     if(count == 0){
         cout << "TT";
     }else{
