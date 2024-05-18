5c393f161a3f79077b92f8e92c44fbf3199d1dfa
sy0223lee <89008921+sy0223lee@users.noreply.github.com>
2024-05-18 17:00:40 +0900

    [Silver I] Title: IOIOI, Time: 40 ms, Memory: 4916 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.cc
+++ 백준/Silver/5525. IOIOI/IOIOI.cc
@@ -2,35 +2,29 @@
 using namespace std;
 
 int main(){
-    int N; // I와 O가 교대로 나오는 문자열
+    int N; // 찾을 문자열의 길이 = 2*N+1
     int M; // 입력 문자열 길이
     string S; // 입력 문자열
-    string P = ""; // 찾을 문자열
-    int count = 0; // P의 개수
+    int ans = 0; // P의 개수
 
     cin >> N >> M >> S;
-    for(int i=0; i<2*N+1; i++){
-        if(i%2 == 0){
-            P += "I";
-        }else{
-            P += "O";
-        }
-    }
 
-    for(int i=0; i<M; i++){
-        bool flag = true;
-        for(int j=0; j<P.length(); j++){
-            if(S[i+j] != P[j]){
-                flag = false;
-                break;
+    // I를 찾은 후 OI의 개수를 세고 N과 일치하는 경우 ans++
+    for(int i=0; i<M-2*N; i++){
+        if(S[i] == 'I'){
+            int cnt = 0;
+            while(S[i+1] == 'O' && S[i+2] == 'I'){
+                cnt++;
+                i += 2; // 마지막으로 찾은 I 위치로 index 변경하기
+                if(cnt == N){
+                    ans++;
+                    cnt--; // 다음에도 OI가 나오면 다음 것도 ans++ 해야하므로 이를 계산하기 위해 cnt--
+                }
             }
         }
-        if(flag == true){
-            count++;
-        }
     }
 
-    cout << count;  
+    cout << ans;  
     
     return 0;
 }
