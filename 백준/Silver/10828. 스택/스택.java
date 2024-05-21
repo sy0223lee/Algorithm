749065699304241363445a81a2f7dfba7e542793
sy0223lee <89008921+sy0223lee@users.noreply.github.com>
2024-05-21 16:44:12 +0900

    [Silver IV] Title: 스택, Time: 364 ms, Memory: 20344 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,48 +1,74 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
+class MyStack{
+	// 데이터를 저장할 수 있는 int형 배열이 멤버 변수로 정의
+	private int[] stack;
+	private int index;	
+	
+	public MyStack(int size) {
+		this.stack = new int[size];
+		this.index = -1;
+	}
+	// push, pop, top, empty, size 메소드 정의
+	public void push(int num) {
+		stack[++index] = num;
+	}
+	public int pop() {
+		if(index < 0) {
+			return -1;
+		}else {
+			int num = stack[index];
+			stack[index--] = 0;
+			return num;
+		}
+	}
+	public int top() {
+		if(index < 0) {
+			return -1;
+		}else {
+			return stack[index];
+		}
+	}
+	public int empty() {
+		if(index < 0) {
+			return 1;
+		}else {
+			return 0;
+		}
+	}
+	public int size() {
+		return index + 1;
+	}
+}
+
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-      
-        int cmdCount = Integer.parseInt(br.readLine());
-        int[] stack = new int[cmdCount];
-        int index = -1;  
-
-        for(int i=0; i<cmdCount; i++){
-            String[] cmd = br.readLine().split(" ");
-
-            switch (cmd[0]) {
-                case "push":
-                    stack[++index] = Integer.parseInt(cmd[1]);
-                    break;
-                case "pop":
-                    if(index < 0)
-                        System.out.println(-1);
-                    else{
-                        System.out.println(stack[index]);
-                        stack[index--] = 0;
-                    }
-                    break;
-                case "size":
-                    System.out.println(index + 1);
-                    break;
-                case "empty":
-                    if(index < 0)
-                        System.out.println(1);
-                    else
-                        System.out.println(0);
-                    break;
-                case "top":
-                    if(index < 0)
-                        System.out.println(-1);
-                    else{
-                        System.out.println(stack[index]);
-                    }
-                    break;            
-                default:
-                    break;
-            }
-        }
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int N = Integer.parseInt(br.readLine());
+		MyStack st = new MyStack(N);
+		
+		for(int i=0; i<N; i++) {
+			String[] cmd = br.readLine().split(" ");
+			switch (cmd[0]) {
+			case "push":
+				st.push(Integer.parseInt(cmd[1]));
+				break;
+			case "pop":
+				System.out.println(st.pop());
+				break;
+			case "size":
+				System.out.println(st.size());
+				break;
+			case "empty":
+				System.out.println(st.empty());
+				break;
+			case "top":
+				System.out.println(st.top());
+				break;
+			}
+		}
+	}
 }
