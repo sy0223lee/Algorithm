78543ee8f5bd5b875f6629e7291ff21e1dc34622
sy0223lee <89008921+sy0223lee@users.noreply.github.com>
2024-10-18 15:25:01 +0900

    [Silver I] Title: 안전 영역, Time: 312 ms, Memory: 57124 KB -BaekjoonHub

---
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -0,0 +1,74 @@
+import java.io.*;
+import java.util.*;
+
+class Point{
+    public int x;
+    public int y;
+
+    Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
+
+public class Main {
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {1, 0, -1, 0};
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] map = new int[N][N];
+        int maxH = 0;
+        int answer = 0;
+
+        for(int i=0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<N; j++){
+                int h = Integer.parseInt(st.nextToken());
+                map[i][j] = h;
+                maxH = Math.max(maxH, h);
+            }
+        }
+
+        for(int h=0; h<maxH; h++){ // 홍수 높이
+            Queue<Point> q = new LinkedList<>();
+            boolean[][] visit = new boolean[N][N];
+            int cnt = 0;
+
+            for(int i=0; i<N; i++){
+                for(int j=0; j<N; j++){
+                    if(map[i][j] > h && !visit[i][j]){ // 처음 만나는 구역인 경우
+                        cnt++;
+                        q.add(new Point(i, j));
+                        visit[i][j] = true;
+                    }
+
+                    while(!q.isEmpty()){
+                        Point point = q.poll();
+                        int x = point.x;
+                        int y = point.y;
+        
+                        for(int k=0; k<4; k++){
+                            int nx = x + dx[k];
+                            int ny = y + dy[k];
+        
+                            if(nx < 0 || nx >= N || ny < 0 || ny >= N){
+                                continue;
+                            }
+        
+                            if(map[nx][ny] > h && !visit[nx][ny]){
+                                q.add(new Point(nx, ny));
+                                visit[nx][ny] = true;
+                            }
+                        }
+                    }
+        
+                    answer = Math.max(answer, cnt);
+                }
+            }            
+        }
+        System.out.println(answer);
+    }
+}
