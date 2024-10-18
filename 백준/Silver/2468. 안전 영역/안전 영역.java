eee34d5e8a582b23ea9442d01df555009c47ff76
sy0223lee <89008921+sy0223lee@users.noreply.github.com>
2024-10-18 15:31:37 +0900

    [Silver I] Title: 안전 영역, Time: 260 ms, Memory: 55732 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.java
+++ 백준/Silver/2468. 안전 영역/안전 영역.java
@@ -14,12 +14,16 @@ class Point{
 public class Main {
     static int[] dx = {0, 1, 0, -1};
     static int[] dy = {1, 0, -1, 0};
+    static int[][] map;
+    static boolean[][] visit;
+    static int N;
+
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        int N = Integer.parseInt(br.readLine());
-        int[][] map = new int[N][N];
+        N = Integer.parseInt(br.readLine());
+        map = new int[N][N];
         int maxH = 0;
         int answer = 0;
 
@@ -33,42 +37,45 @@ public class Main {
         }
 
         for(int h=0; h<maxH; h++){ // 홍수 높이
-            Queue<Point> q = new LinkedList<>();
-            boolean[][] visit = new boolean[N][N];
+            visit = new boolean[N][N];
             int cnt = 0;
 
             for(int i=0; i<N; i++){
                 for(int j=0; j<N; j++){
                     if(map[i][j] > h && !visit[i][j]){ // 처음 만나는 구역인 경우
                         cnt++;
-                        q.add(new Point(i, j));
-                        visit[i][j] = true;
-                    }
-
-                    while(!q.isEmpty()){
-                        Point point = q.poll();
-                        int x = point.x;
-                        int y = point.y;
-        
-                        for(int k=0; k<4; k++){
-                            int nx = x + dx[k];
-                            int ny = y + dy[k];
-        
-                            if(nx < 0 || nx >= N || ny < 0 || ny >= N){
-                                continue;
-                            }
-        
-                            if(map[nx][ny] > h && !visit[nx][ny]){
-                                q.add(new Point(nx, ny));
-                                visit[nx][ny] = true;
-                            }
-                        }
-                    }
-        
+                        BFS(i, j, h);
+                    }        
                     answer = Math.max(answer, cnt);
                 }
             }            
         }
         System.out.println(answer);
+    }
+
+    static void BFS(int i, int j, int h){
+        Queue<Point> q = new LinkedList<>();
+        q.add(new Point(i, j));
+        visit[i][j] = true;
+
+        while(!q.isEmpty()){
+            Point point = q.poll();
+            int x = point.x;
+            int y = point.y;
+
+            for(int k=0; k<4; k++){
+                int nx = x + dx[k];
+                int ny = y + dy[k];
+
+                if(nx < 0 || nx >= N || ny < 0 || ny >= N){
+                    continue;
+                }
+
+                if(map[nx][ny] > h && !visit[nx][ny]){
+                    q.add(new Point(nx, ny));
+                    visit[nx][ny] = true;
+                }
+            }
+        }
     }
 }
